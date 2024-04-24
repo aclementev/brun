d540a394ab92c7ed99baea2952bf612ac28c3008
Alvaro Clemente <clemente.varo@gmail.com>
2024-04-24 22:31:15 +0200

    fix: simplify the structures

--- src/commit.rs
+++ src/commit.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct UserInfo {
     name: String,
@@ -7,53 +8,40 @@ pub struct UserInfo {
     date: String,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Tree {
     sha: String,
-    url: String,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Verification {
-    verified: bool,
-    reason: String,
-    signature: Option<String>,
-    payload: Option<String>,
-}
-
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Commit {
     author: UserInfo,
     committer: UserInfo,
     message: String,
     tree: Tree,
-    url: String,
-    comment_count: u64,
-    verification: Verification,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct User {
     login: String,
-    id: u64,
-    node_id: String,
-    r#type: String,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct Ref {
     sha: String,
     url: String,
-    html_url: String,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 pub struct CommitResponse {
     pub sha: String,
-    node_id: String,
     commit: Commit,
     url: String,
-    html_url: String,
     author: User,
     committer: User,
     parents: Vec<Ref>,
